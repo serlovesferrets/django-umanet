4064c222ba3bbd3aec2506829238bb4e301cb3df
ser <140543954+serlovesferrets@users.noreply.github.com>
2024-12-04 17:50:39 +0100

    finita parte 12

--- primo_progetto_root/primo_progetto/seconda_app/urls.py
+++ primo_progetto_root/primo_progetto/seconda_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path 
-from seconda_app.views import es_if
+from seconda_app.views import es_if, es_for
 
 app_name = "seconda_app"
 
 urlpatterns = [
-    path("es_if", es_if, name="es_if")
+    path("es_if", es_if, name="es_if"),
+    path("es_for", es_for, name="es_for")
 ]
--- primo_progetto_root/primo_progetto/seconda_app/views.py
+++ primo_progetto_root/primo_progetto/seconda_app/views.py
@@ -1,3 +1,4 @@
+import datetime
 from django.shortcuts import render
 
 def es_if(request):
@@ -7,3 +8,14 @@ def es_if(request):
         "var3": 300
     }
     return render(request, "seconda_app/es_if.html", context)
+
+def es_for(request):
+    context = {
+        "list1": [1, datetime.date(2019, 7, 16), "don't give up"],
+        "list2": [1, datetime.date(2019, 7, 16), "don't give up"],
+        "my_dict": {
+            "chiave1" : "Valore1",
+            "chiave2" : "Valore2"
+        }
+    }
+    return render(request, "seconda_app/es_for.html", context)
